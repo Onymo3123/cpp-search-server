3e49ff7b2a01665240f207b6592c0c34567df76e
Onym0 <62027@mail.ru>
2022-12-11 13:37:07 +0300

    Tests ver0.1

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -38,45 +38,173 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
 /*
 Разместите код остальных тестов здесь
 */
-   void TestExcludeRelevance() {
+ void TestComputeRelevance() {
     
-    
+        double check[3] = {0.274653,0.101366,0.101366};
         SearchServer search_server;
+        search_server.SetStopWords("и в на"s);
+
         search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, -3});
         search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
-        search_server.AddDocument(2, "ухоженный кот выразительные глаза"s, DocumentStatus::ACTUAL,
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL,
                               {5, -12, 2, 1});
-   
-        for (const Document& document : search_server.FindTopDocuments("кот"s)) {
-             ASSERT(document.relevance == 0);
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s)) {
+            if(a < 3){
+                ASSERT(abs(check[a]-document.relevance)<1e-6);    
+            }
+             a++;   
         }
         
-      
     }
-    void TestExcludeRating() {
+     
+    void TestComputeRating() {
     
-    
+        double check[3] = {5,8,-3};
         SearchServer search_server;
-        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, -3});
-        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
-        search_server.AddDocument(2, "ухоженный кот выразительные глаза"s, DocumentStatus::ACTUAL,
-                              {5, -12, 2, 1});
-        int a = 999;
-        for (const Document& document : search_server.FindTopDocuments("кот"s)) {
-            ASSERT(document.rating < a);
-            a = document.rating;
+        search_server.SetStopWords("и в на"s);
+
+        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, 8, 8, 8, 8});
+        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {-7, -1, -2, -3, -4});
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL,
+                              {5, -12, 2, 1, 15, 20});
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s)) {
+            if(a < 3){
+                ASSERT(abs(check[a]-document.rating)<1e-6);    
+            }
+             a++;   
         }
         
-      
-     }
+    } 
+    
+       
+    void TestComputeStatActual() {
+    
+       int id = 0;
+        SearchServer search_server;
+        search_server.SetStopWords("и в на"s);
+
+        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, 8, 8, 8, 8});
+        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::IRRELEVANT, {-7, -1, -2, -3, -4});
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::BANNED,
+                              {5, -12, 2, 1, 15, 20});
+        search_server.AddDocument(3, "ухоженный пёс выразительные кот глаза"s, DocumentStatus::REMOVED,
+                              {5, -12, 2, 1, 15, 20});
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s, DocumentStatus::ACTUAL)) {
+            if(a < 1){
+                ASSERT(abs(id-document.id)<1e-6);  
+            }
+             a++;   
+        }
+        
+    } 
+    
+    void TestComputeStatIrrelevant() {
+    
+        int id = 1;
+        SearchServer search_server;
+        search_server.SetStopWords("и в на"s);
+
+        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, 8, 8, 8, 8});
+        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::IRRELEVANT, {-7, -1, -2, -3, -4});
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::BANNED,
+                              {5, -12, 2, 1, 15, 20});
+        search_server.AddDocument(3, "ухоженный пёс выразительные кот глаза"s, DocumentStatus::REMOVED,
+                              {5, -12, 2, 1, 15, 20});
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s, DocumentStatus::IRRELEVANT)) {
+            if(a < 1){
+                ASSERT(abs(id-document.id)<1e-6);  
+            }
+             a++;   
+        }
+        
+    }
+    
+    void TestComputeStatBanned() {
+    
+        int id = 2;
+        SearchServer search_server;
+        search_server.SetStopWords("и в на"s);
+        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, 8, 8, 8, 8});
+        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::IRRELEVANT, {-7, -1, -2, -3, -4});
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::BANNED,
+                              {5, -12, 2, 1, 15, 20});
+        search_server.AddDocument(3, "ухоженный пёс выразительные кот глаза"s, DocumentStatus::REMOVED,
+                              {5, -12, 2, 1, 15, 20});
+    
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s, DocumentStatus::BANNED)) {
+            if(a < 1){
+                ASSERT(abs(id-document.id)<1e-6);
+            }
+             a++;   
+        }
+        
+    }
+    
+    void TestComputeStatRemoved() {
+    
+        int id = 3;
+        SearchServer search_server;
+        search_server.SetStopWords("и в на"s);
+        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, 8, 8, 8, 8});
+        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::IRRELEVANT, {-7, -1, -2, -3, -4});
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::BANNED,
+                              {5, -12, 2, 1, 15, 20});
+        search_server.AddDocument(3, "ухоженный пёс выразительные кот глаза"s, DocumentStatus::REMOVED,
+                              {5, -12, 2, 1, 15, 20});
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s, DocumentStatus::REMOVED)) {
+            if(a < 1){
+                ASSERT(abs(id-document.id)<1e-6); 
+            }
+             a++;   
+        }
+        
+    }
+    
+    void TestCastomPredict() {
+    
+        int id = 2;
+        SearchServer search_server;
+        search_server.SetStopWords("и в на"s);
+        search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, {8, 8, 8, 8, 8});
+        search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::IRRELEVANT, {-7, -1, -2, -3, -4});
+        search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::BANNED,
+                              {5, -12, 2, 1, 15, 20});
+        search_server.AddDocument(3, "ухоженный пёс выразительные кот глаза"s, DocumentStatus::REMOVED,
+                              {5, -12, 2, 1, 15, 20});
+        int a = 0;
+        for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s, 
+               [](int document_id, DocumentStatus status, int rating) { return document_id ==  2; })) {
+            if(a < 1){
+                ASSERT(abs(id-document.id)<1e-6);   
+            }
+             a++;   
+        }
+        
+    }
+    
+    
+
+   
 
 
 
 // Функция TestSearchServer является точкой входа для запуска тестов
 void TestSearchServer() {
     RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
-    RUN_TEST(TestExcludeRelevance);
-    RUN_TEST(TestExcludeRating);
+    RUN_TEST(TestComputeRelevance);
+    RUN_TEST(TestComputeRating);
+    RUN_TEST(TestComputeStatActual);
+    RUN_TEST(TestComputeStatIrrelevant);
+    RUN_TEST(TestComputeStatBanned);
+    RUN_TEST(TestComputeStatRemoved);
+    RUN_TEST(TestCastomPredict);
+    
     // Не забудьте вызывать остальные тесты здесь
 }
 
